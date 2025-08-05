d059e2f0dfbda760109b83cc7e8fea7dfa7872c6
hrushi18-lang <toghitihrushikesh@gmail.com>
2025-08-05 10:46:19 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,10 @@
 {"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
